a57dde2ac4087d78ddf8c1cc2872f0953a79b146
Anthony Underwood <email2ants@gmail.com>
2010-09-06 16:00:15 +0100

    initial adding and testing of jScrollPanel

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function()
+{
+  $('#message_panel').jScrollPane(
+    {
+      showArrows: true,
+      horizontalGutter: 10
+    }
+  );
+});
